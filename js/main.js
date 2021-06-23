1f736bc6b0b6bb2ac62e071e0c704d96cc6a2bfa
Jhon Camargo <jhonalexcamargo07@gmail.com>
2021-06-22 19:06:51 -0500

    Falta ajustar algunas cosas para hacerlo responsive, agregar iconos al nav y agregar los lenguejes.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 let parallax = document.querySelector('.parallax__imagen');
 let parallax__titulo = document.querySelector('.parallax__titulo');
+var contenedorP = document.getElementById('contenedor_parallax'),
+    header = document.getElementById('header');
+    altura = contenedorP.offsetTop,
+    iconoSubir = document.getElementById('icono_subir'),
+    icono_nav = document.getElementById('icono_nav');
 
 function scrollParallax(){
     let scrollTop = document.documentElement.scrollTop;
@@ -8,3 +13,19 @@ function scrollParallax(){
 }
 
 window.addEventListener('scroll', scrollParallax);
+
+var modificarNav = function(){
+    if(document.documentElement.scrollTop > 10){
+        header.classList.add('header2');
+        contenedorP.style.marginTop = altura - 40 + "px";
+        iconoSubir.classList.remove("icono__none");
+        iconoSubir.classList.add("icono__flex");
+    }else{
+        header.classList.remove('header2');
+        contenedorP.style.marginTop = "0px";
+        iconoSubir.classList.remove("icono__flex");
+        iconoSubir.classList.add("icono__none");
+    }
+}
+
+window.addEventListener('scroll', modificarNav);
