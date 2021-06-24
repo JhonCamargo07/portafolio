8332f9f98051d4c97b3c9d52a41b290cbf69b807
Jhon Camargo <jhonalexcamargo07@gmail.com>
2021-06-24 18:53:29 -0500

    Se agregó las habilidades y algunas otras correcciones.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@
         header = document.getElementById('header');
         altura = contenedorP.offsetTop,
         iconoSubir = document.getElementById('icono_subir'),
-        icono_nav = document.getElementById('icono_nav');
+        icono_nav = document.getElementById('icono_nav'),
+        navegador = document.querySelector('.header__nav');
 
 
     // Función para la imagen
@@ -50,79 +51,17 @@
 
     //*--------------------------- Desplegar menú ---------------------------
 
-    icono_nav.addEventListener("click", () => {
-        if(icono_nav.className = "fa-bars"){
-            icono_nav.classList.remove("icono icono__nav fas fa-bars");
-            icono_nav.classList.add("icono icono__nav fasfa-times");
-        }else{
-            icono_nav.classList.add("icono icono__nav fas fa-bars");
-            icono_nav.classList.remove("icono icono__nav fas fa-times");
-        }
-    });
-
-    //*------------------------------------------------------------
-    //!--------------------------- Validacion del formulario ---------------------------
-    //*------------------------------------------------------------
-
-    const formulario = document.getElementById('formulario');
-    const inputs = document.querySelectorAll('#formulario input, textarea');
-
-    const expresiones = {
-        nombre: /^[a-zA-ZÀ-ÿ\s]{5,50}$/, // Letras y espacios, pueden llevar acentos.
-        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-        mensaje: /^[a-zA-ZÀ-ÿ0-9_.+-\s/¿?!:@?]{1,10000}$/
-    }
-
-    const validarFormulario = (e) => {
-        switch(e.target.name){
-            case "nombre":
-                validarCampo(expresiones.nombre, e.target, 'nombre');
-            break;
-
-            case "correo":
-                validarCampo(expresiones.correo, e.target, 'correo');
-            break;
-
-            case "mensaje":
-                if(expresiones.mensaje.test(e.target.value)){
-                    document.getElementById('grupo__mensaje').classList.remove('formulario__grupo-textarea-incorrecto');
-                    document.getElementById('grupo__mensaje').classList.add('formulario__grupo-textarea-correcto');
-                    document.querySelector('#grupo__mensaje i').classList.add('fa-check-circle');
-                    document.querySelector('#grupo__mensaje i').classList.remove('fa-times-circle');
-                    document.querySelector('#grupo__mensaje .formulario__input-error').classList.remove('formulario__input-error-activo');
-                }else{
-                    document.getElementById('grupo__mensaje').classList.add('formulario__grupo-textarea-incorrecto');
-                    document.getElementById('grupo__mensaje').classList.remove('formulario__grupo-textarea-correcto');
-                    document.querySelector('#grupo__mensaje i').classList.add('fa-times-circle');
-                    document.querySelector('#grupo__mensaje i').classList.remove('fa-check-circle');
-                    document.querySelector('#grupo__mensaje .formulario__input-error').classList.add('formulario__input-error-activo');
-                }
-            break;
-        }
-    }
-
-    const validarCampo = (expresion, input, campo) => {
-        if(expresion.test(input.value)){
-            document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-            document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-            document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-            document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-            document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-        }else{
-            document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-            document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-            document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-            document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-            document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-        }
-    }
-
-    inputs.forEach((input) => {
-        input.addEventListener("keyup", validarFormulario);
-        input.addEventListener("blur", validarFormulario);
-    });
-
-    formulario.addEventListener("submit", (e) => {
-        e.preventDefault();
-    });
+    // icono_nav.addEventListener("click", function() {
+    //     if(icono_nav.className === 'icono icono__nav fas fa-bars'){
+    //         icono_nav.classList.remove("fa-bars");
+    //         icono_nav.classList.add("fa-times");
+    //         navegador.classList.remove('header__nav2-oculto');
+    //         navegador.classList.add('header__nav2');
+    //     }else{
+    //         icono_nav.classList.add("fa-bars");
+    //         icono_nav.classList.remove("fa-times");
+    //         navegador.classList.remove('header__nav2');
+    //         navegador.classList.add('header__nav2-oculto');
+    //     }
+    // });
 }())
---
+++ js/validacion.js
@@ -0,0 +1,87 @@
+(function(){
+    //*------------------------------------------------------------
+    //!--------------------------- Validacion del formulario ---------------------------
+    //*------------------------------------------------------------
+
+    const formulario = document.getElementById('formulario');
+    const inputs = document.querySelectorAll('#formulario input, textarea');
+
+    const expresiones = {
+        nombre: /^[a-zA-ZÀ-ÿ\s]{5,50}$/, // Letras y espacios, pueden llevar acentos.
+        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        mensaje: /^[a-zA-ZÀ-ÿ0-9_.+-\s/¿?!:@?]{1,10000}$/
+    }
+
+    const campos = {
+        nombre: false,
+        correo: false,
+        mensaje: false
+    }
+
+    const validarFormulario = (e) => {
+        switch(e.target.name){
+            case "nombre":
+                validarCampo(expresiones.nombre, e.target, 'nombre');
+            break;
+
+            case "correo":
+                validarCampo(expresiones.correo, e.target, 'correo');
+            break;
+
+            case "mensaje":
+                if(expresiones.mensaje.test(e.target.value)){
+                    document.getElementById('grupo__mensaje').classList.remove('formulario__grupo-textarea-incorrecto');
+                    document.getElementById('grupo__mensaje').classList.add('formulario__grupo-textarea-correcto');
+                    document.querySelector('#grupo__mensaje i').classList.add('fa-check-circle');
+                    document.querySelector('#grupo__mensaje i').classList.remove('fa-times-circle');
+                    document.querySelector('#grupo__mensaje .formulario__input-error').classList.remove('formulario__input-error-activo');
+                }else{
+                    document.getElementById('grupo__mensaje').classList.add('formulario__grupo-textarea-incorrecto');
+                    document.getElementById('grupo__mensaje').classList.remove('formulario__grupo-textarea-correcto');
+                    document.querySelector('#grupo__mensaje i').classList.add('fa-times-circle');
+                    document.querySelector('#grupo__mensaje i').classList.remove('fa-check-circle');
+                    document.querySelector('#grupo__mensaje .formulario__input-error').classList.add('formulario__input-error-activo');
+                }
+            break;
+        }
+    }
+
+    const validarCampo = (expresion, input, campo) => {
+        if(expresion.test(input.value)){
+            document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+            document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+            document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+            document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+            document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        }else{
+            document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+            document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+            document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+            document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+            document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        }
+    }
+
+    inputs.forEach((input) => {
+        input.addEventListener("keyup", validarFormulario);
+        input.addEventListener("blur", validarFormulario);
+    });
+
+    formulario.addEventListener("submit", (e) => {
+        if(campos.nombre && campos.correo && campos.mensaje){
+            document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+            
+            setTimeout( () => {
+                document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+            }, 5000);
+
+            document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+                icono.classList.remove('formulario__grupo-correcto');
+		    });
+            
+        }else{
+            document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        }
+        e.preventDefault();
+    });
+}())
