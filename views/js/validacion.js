db32ffda775fb44677aee20d4d60776ea448b378
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2022-09-20 16:45:36 -0500

    Se hicieron ajustes en la validacion del formulario, ahora al darle enviar y que los datos esten vacios, marca con error cada uno de ellos

--- views/js/validacion.js
+++ views/js/validacion.js
@@ -1,11 +1,12 @@
-(function(){
+//*------------------------------------------------------------
+//!--------------------------- Validacion del formulario ---------------------------
+//*------------------------------------------------------------
 
-    //*------------------------------------------------------------
-    //!--------------------------- Validacion del formulario ---------------------------
-    //*------------------------------------------------------------
+(function () {
 
     const formulario = document.getElementById('formulario');
     const inputs = document.querySelectorAll('#formulario input, textarea');
+    const form = document.getElementById('formulario');
 
     const expresiones = {
         nombre: /^[a-zA-ZÀ-ÿ\s]{7,60}$/, // Letras y espacios, pueden llevar acentos.
@@ -22,95 +23,112 @@
     }
 
     const validarFormulario = (e) => {
-        switch(e.target.name){
+        switch (e.target.name) {
             case "nombre":
                 validarCampo(expresiones.nombre, e.target, 'nombre');
-            break;
+                break;
 
             case "correo":
                 validarCampo(expresiones.correo, e.target, 'correo');
                 break;
-                
+
             case "asunto":
-                    validarCampo(expresiones.asunto, e.target, 'asunto');
-            break;
+                validarCampo(expresiones.asunto, e.target, 'asunto');
+                break;
 
             case "mensaje":
-                if(expresiones.mensaje.test(mensaje.value)){
-                    document.getElementById('grupo__mensaje').classList.remove('formulario__grupo-textarea-incorrecto');
-                    document.getElementById('grupo__mensaje').classList.add('formulario__grupo-textarea-correcto');
-                    document.querySelector('#grupo__mensaje i').classList.add('fa-check-circle');
-                    document.querySelector('#grupo__mensaje i').classList.remove('fa-times-circle');
-                    document.querySelector('#grupo__mensaje .formulario__input-error').classList.remove('formulario__input-error-activo');
+                if (expresiones.mensaje.test(mensaje.value)) {
+                    inputMsgCorrecto();
                     campos.mensaje = true;
-                }else{
-                    document.getElementById('grupo__mensaje').classList.add('formulario__grupo-textarea-incorrecto');
-                    document.getElementById('grupo__mensaje').classList.remove('formulario__grupo-textarea-correcto');
-                    document.querySelector('#grupo__mensaje i').classList.add('fa-times-circle');
-                    document.querySelector('#grupo__mensaje i').classList.remove('fa-check-circle');
-                    document.querySelector('#grupo__mensaje .formulario__input-error').classList.add('formulario__input-error-activo');
+                } else {
+                    inputMsgIncorrecto();
                     campos.mensaje = false;
                 }
-            break;
+                break;
         }
     }
 
     const validarCampo = (expresion, input, campo) => {
-        if(expresion.test(input.value)){
-            document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-            document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-            document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-            document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-            document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        if (expresion.test(input.value)) {
+            inputCorrecto(campo);
             campos[campo] = true;
-        }else{
-            document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-            document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-            document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-            document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-            document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        } else {
+            inputIncorrecto(campo);
             campos[campo] = false;
         }
     }
 
+    const inputCorrecto = (nameInput) => {
+        document.getElementById(`grupo__${nameInput}`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${nameInput}`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${nameInput} i`).classList.add('fa-check-circle');
+        document.querySelector(`#grupo__${nameInput} i`).classList.remove('fa-times-circle');
+        document.querySelector(`#grupo__${nameInput} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+    }
+
+    const inputIncorrecto = (nameInput) => {
+        document.getElementById(`grupo__${nameInput}`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${nameInput}`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${nameInput} i`).classList.add('fa-times-circle');
+        document.querySelector(`#grupo__${nameInput} i`).classList.remove('fa-check-circle');
+        document.querySelector(`#grupo__${nameInput} .formulario__input-error`).classList.add('formulario__input-error-activo');
+    }
+
+    const inputMsgCorrecto = () => {
+        document.getElementById('grupo__mensaje').classList.remove('formulario__grupo-textarea-incorrecto');
+        document.getElementById('grupo__mensaje').classList.add('formulario__grupo-textarea-correcto');
+        document.querySelector('#grupo__mensaje i').classList.add('fa-check-circle');
+        document.querySelector('#grupo__mensaje i').classList.remove('fa-times-circle');
+        document.querySelector('#grupo__mensaje .formulario__input-error').classList.remove('formulario__input-error-activo');
+    }
+
+    const inputMsgIncorrecto = () => {
+        document.getElementById('grupo__mensaje').classList.add('formulario__grupo-textarea-incorrecto');
+        document.getElementById('grupo__mensaje').classList.remove('formulario__grupo-textarea-correcto');
+        document.querySelector('#grupo__mensaje i').classList.add('fa-times-circle');
+        document.querySelector('#grupo__mensaje i').classList.remove('fa-check-circle');
+        document.querySelector('#grupo__mensaje .formulario__input-error').classList.add('formulario__input-error-activo');
+    }
+
+
     inputs.forEach((input) => {
         input.addEventListener("keyup", validarFormulario);
         input.addEventListener("blur", validarFormulario);
-        // input.addEventListener("keydown", validarFormulario);
     });
-    
-    // formulario.addEventListener('submit', (e) => {
-    //     if(campos.nombre == false){
-    //         campos[nombre] = false;
-    //         console.log(campos.nombre);
-    //         validarCampo(expresiones.nombre, e.target, 'nombre');
-    //     }else if(campos.nombre === false){
-    //         validarCampo(expresiones.nombre, e.target, 'nombre');
-    //     }
-    // });
+
     formulario.addEventListener('submit', (e) => {
-        if(campos.nombre && campos.correo && campos.asunto && campos.mensaje){
-            // formulario.reset();
+        if (campos.nombre && campos.correo && campos.asunto && campos.mensaje) {
+
             document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
             document.getElementById('grupo__mensaje').classList.remove('formulario__grupo-textarea-correcto');
             document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-            
-            setTimeout( () => {
+
+            setTimeout(() => {
                 document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
             }, 5000);
 
             document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
                 icono.classList.remove('formulario__grupo-correcto');
             });
-            
+
             campos[nombre] = false;
             campos[correo] = false;
             campos[asunto] = false;
             campos[mensaje] = false;
 
-        }else{
+        } else {
             e.preventDefault();
+            inputs.forEach((input) => {
+                if (input.value == '') {
+                    if (input.name === "mensaje") {
+                        inputMsgIncorrecto();
+                    } else {
+                        inputIncorrecto(input.name);
+                    }
+                }
+            });
             document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
         }
     });
+
 }())
