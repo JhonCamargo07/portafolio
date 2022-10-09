9c775d185c90577ec2d0915aae4a3ddb0ecdda60
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2022-10-09 14:34:09 -0500

    Ajustes a react router

--- views/src/router/Router.jsx
+++ views/src/router/Router.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Error404 from '../pages/Error404';
 
 export default function Router() {
 	return (
 		<>
-			<BrowserRouter>
+			<HashRouter>
 				<Routes>
 					<Route index element={<Home />} />
 					<Route path="/" element={<Home />} />
 					<Route path="*" element={<Error404 />} />
-					<Route path="/error404" element={<Error404 />} />
 					<Route path="/incomplete" element={<Error404 />} />
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</>
 	);
 }
